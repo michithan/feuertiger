37400d503fbb3fe7c4ae0729440510415aebb4d8
MonoMichi <michael.thanei@gmail.com>
2019-12-10 19:12:50 +0100

    lint and prettify

--- packages/data-access-firebase/src/mappings/ResolverMap.ts
+++ packages/data-access-firebase/src/mappings/ResolverMap.ts
@@ -1,132 +1,133 @@
 import {
-  IntrospectionSchema,
-  IntrospectionObjectType,
-  IntrospectionField
-} from "graphql";
-import { IResolvers, IResolverObject, IFieldResolver } from "apollo-server";
+    IntrospectionSchema,
+    IntrospectionObjectType,
+    IntrospectionField
+} from 'graphql';
+import { IResolvers, IResolverObject, IFieldResolver } from 'apollo-server';
 
 import {
-  filterIntrospectionObjectTypes,
-  isConnectionObjectType
-} from "@feuertiger/utils-graphql";
-import { __schema } from "@feuertiger/schema-graphql/dist/schema.json";
+    filterIntrospectionObjectTypes,
+    isConnectionObjectType
+} from '@feuertiger/utils-graphql';
+import { __schema } from '@feuertiger/schema-graphql/dist/schema.json';
 
-import { ResolveType, IServiceMap } from "./ServiceMap";
-import { resolveObjectResolver, resolveListResolver } from "./ServiceResolver";
-import { IConnectionService } from "../services/ConnectionService";
-import { INodeService } from "../services/NodeService";
+import { ResolveType, IServiceMap } from './ServiceMap';
+import { resolveObjectResolver, resolveListResolver } from './ServiceResolver';
+import { IConnectionService } from '../services/ConnectionService';
+import { INodeService } from '../services/NodeService';
 
 export interface FieldObjectTuple {
-  field: IntrospectionField;
-  fieldObject: IntrospectionObjectType;
+    field: IntrospectionField;
+    fieldObject: IntrospectionObjectType;
 }
 
 export interface FieldObjectMapping {
-  field: IntrospectionField;
-  fieldObject: IntrospectionObjectType;
-  parentObject: IntrospectionObjectType;
-  type: ResolveType;
+    field: IntrospectionField;
+    fieldObject: IntrospectionObjectType;
+    parentObject: IntrospectionObjectType;
+    type: ResolveType;
 }
 
 const graphQLSchema = (__schema as unknown) as IntrospectionSchema;
 const allTypes: IntrospectionObjectType[] = filterIntrospectionObjectTypes(
-  graphQLSchema
+    graphQLSchema
 );
 
 const nameOfFieldType = (field: any) => {
-  const { type } = field;
-  const { ofType } = type || {};
-  const name = type.name || (ofType && ofType.name);
-  return name;
+    const { type } = field;
+    const { ofType } = type || {};
+    const name = type.name || (ofType && ofType.name);
+    return name;
 };
 
 const mapFieldsToObjectType = (
-  fields: readonly IntrospectionField[]
+    fields: readonly IntrospectionField[]
 ): Array<FieldObjectTuple> =>
-  fields
-    .map((field: IntrospectionField) => {
-      const typeName = nameOfFieldType(field);
-      return {
-        field,
-        fieldObject: allTypes.find(
-          object => object.name === typeName
-        ) as IntrospectionObjectType
-      };
-    })
-    .filter(({ fieldObject }) => !!fieldObject);
+    fields
+        .map((field: IntrospectionField) => {
+            const typeName = nameOfFieldType(field);
+            return {
+                field,
+                fieldObject: allTypes.find(
+                    object => object.name === typeName
+                ) as IntrospectionObjectType
+            };
+        })
+        .filter(({ fieldObject }) => !!fieldObject);
 
 const fieldObjectMap: Array<FieldObjectMapping> = allTypes.reduce(
-  (
-    fieldObjectMappings: Array<FieldObjectMapping>,
-    object: IntrospectionObjectType
-  ): Array<FieldObjectMapping> => {
-    const fieldTuples: Array<FieldObjectTuple> = mapFieldsToObjectType(
-      object.fields
-    );
-    for (const fieldTuple of fieldTuples) {
-      const { field, fieldObject } = fieldTuple;
-      const type = isConnectionObjectType(fieldObject)
-        ? ResolveType.Connection
-        : ResolveType.Node;
+    (
+        fieldObjectMappings: Array<FieldObjectMapping>,
+        object: IntrospectionObjectType
+    ): Array<FieldObjectMapping> => {
+        const fieldTuples: Array<FieldObjectTuple> = mapFieldsToObjectType(
+            object.fields
+        );
+        for (const fieldTuple of fieldTuples) {
+            const { field, fieldObject } = fieldTuple;
+            const type = isConnectionObjectType(fieldObject)
+                ? ResolveType.Connection
+                : ResolveType.Node;
 
-      fieldObjectMappings.push({
-        field,
-        fieldObject,
-        parentObject: object,
-        type
-      });
-    }
+            fieldObjectMappings.push({
+                field,
+                fieldObject,
+                parentObject: object,
+                type
+            });
+        }
 
-    return fieldObjectMappings;
-  },
-  []
+        return fieldObjectMappings;
+    },
+    []
 );
 
 export const createResolvers = (serviceMap: IServiceMap): IResolvers =>
-  fieldObjectMap.reduce<IResolvers>(
-    (
-      resolvers: IResolvers,
-      fieldObjectMapping: FieldObjectMapping
-    ): IResolvers => {
-      const { service, type: serviceType } = serviceMap[
-        fieldObjectMapping.fieldObject.name
-      ];
+    fieldObjectMap.reduce<IResolvers>(
+        (
+            resolvers: IResolvers,
+            fieldObjectMapping: FieldObjectMapping
+        ): IResolvers => {
+            const { service, type: serviceType } = serviceMap[
+                fieldObjectMapping.fieldObject.name
+            ];
 
-      const {
-        field,
-        fieldObject,
-        parentObject,
-        type: fieldType
-      } = fieldObjectMapping;
+            const {
+                field,
+                fieldObject,
+                parentObject,
+                type: fieldType
+            } = fieldObjectMapping;
 
-      if (fieldType !== serviceType) {
-        throw "internal Resolver-Service mapping exception!";
-      }
+            if (fieldType !== serviceType) {
+                throw 'internal Resolver-Service mapping exception!';
+            }
 
-      const objectName = parentObject.name;
-      const fieldName = field.name;
+            const objectName = parentObject.name;
+            const fieldName = field.name;
 
-      const resolverObject = (resolvers[objectName] || {}) as IResolverObject;
+            const resolverObject = (resolvers[objectName] ||
+                {}) as IResolverObject;
 
-      if (serviceType === ResolveType.Connection) {
-        resolverObject[fieldName] = resolveListResolver(
-          field,
-          fieldObject,
-          parentObject,
-          service as IConnectionService
-        );
-      } else {
-        resolverObject[fieldName] = resolveObjectResolver(
-          field,
-          fieldObject,
-          parentObject,
-          service as INodeService
-        );
-      }
+            if (serviceType === ResolveType.Connection) {
+                resolverObject[fieldName] = resolveListResolver(
+                    field,
+                    fieldObject,
+                    parentObject,
+                    service as IConnectionService
+                );
+            } else {
+                resolverObject[fieldName] = resolveObjectResolver(
+                    field,
+                    fieldObject,
+                    parentObject,
+                    service as INodeService
+                );
+            }
 
-      resolvers[objectName] = resolverObject;
+            resolvers[objectName] = resolverObject;
 
-      return resolvers;
-    },
-    {}
-  );
+            return resolvers;
+        },
+        {}
+    );
--- scripts/linkdist.js
+++ scripts/linkdist.js
@@ -1,28 +1,28 @@
-const fs = require("fs");
-const { getPackages } = require("@lerna/project");
+const fs = require('fs');
+const { getPackages } = require('@lerna/project');
 
 const linkscript = `export * from "../src/index.ts";`;
 
 const getPackagePath = name => {
-  const packageJsonPath = require.resolve(`${name}/package.json`);
-  return packageJsonPath.split("package.json")[0];
+    const packageJsonPath = require.resolve(`${name}/package.json`);
+    return packageJsonPath.split('package.json')[0];
 };
 
 (async () => {
-  const cwd = process.cwd();
-  const pkgs = await getPackages(cwd);
+    const cwd = process.cwd();
+    const pkgs = await getPackages(cwd);
 
-  for (pkg of pkgs) {
-    const path = getPackagePath(pkg.name);
-    const distpath = `${path}dist`;
-    const distpathindex = `${distpath}/index.js`;
+    for (pkg of pkgs) {
+        const path = getPackagePath(pkg.name);
+        const distpath = `${path}dist`;
+        const distpathindex = `${distpath}/index.js`;
 
-    fs.existsSync(distpath) || fs.mkdirSync(distpath);
-    fs.writeFileSync(distpathindex, linkscript, {
-      encoding: "utf8",
-      flag: "w"
-    });
+        fs.existsSync(distpath) || fs.mkdirSync(distpath);
+        fs.writeFileSync(distpathindex, linkscript, {
+            encoding: 'utf8',
+            flag: 'w'
+        });
 
-    console.log(`linked "${distpathindex}" to "../src/index.ts"`);
-  }
+        console.log(`linked "${distpathindex}" to "../src/index.ts"`);
+    }
 })();
