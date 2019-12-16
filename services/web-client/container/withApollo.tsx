e7421176d461321baa9e5aeb03f0b211424bda94
MonoMichi <michael.thanei@gmail.com>
2019-12-16 19:55:42 +0100

    replaced localstorage token with authsingleton

--- services/web-client/container/withApollo.tsx
+++ services/web-client/container/withApollo.tsx
@@ -5,17 +5,25 @@ import { setContext } from 'apollo-link-context';
 import withApollo from 'next-with-apollo';
 import fetch from 'isomorphic-unfetch';
 
+import { AuthSingleton } from './withAuth';
+
 // Update the GraphQL endpoint to any instance of GraphQL that you like
 const GRAPHQL_URL = 'http://localhost:4000/';
 
-const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token || ''
-        }
-    };
+const authLink = setContext(async (_, { headers }) => {
+    try {
+        const token = await new AuthSingleton().firebaseAuth.currentUser.getIdToken();
+        return {
+            headers: {
+                ...headers,
+                authorization: token
+            }
+        };
+    } catch (error) {
+        return {
+            headers
+        };
+    }
 });
 
 const httpLink = createHttpLink({
