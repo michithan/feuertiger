9dca3c2425d255f8bbef824a3de709fbbe9babab
Monomichi <michael.thanei@gmail.com>
2019-12-17 14:47:38 +0100

    Update versions.js

--- scripts/versions.js
+++ scripts/versions.js
@@ -3,4 +3,4 @@ const { execSync } = require('child_process');
 const commit = execSync('git rev-parse HEAD').toString().trim().substring(0, 7);
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
-execSync(`lerna version --yes --exact --amend --preid=${branch} --cd-version=${commit}`);
+execSync(`lerna version prepatch --yes --exact --amend --preid=${branch} --cd-version=${commit}`);
