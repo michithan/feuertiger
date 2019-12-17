368d5a992cb5e96f7354832a19bae0bbad1007cd
MonoMichi <michael.thanei@gmail.com>
2019-12-17 21:27:31 +0100

    fixed branch names in preid

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,12 +3,12 @@ const { execSync } = require('child_process');
 const commit = execSync('git rev-parse HEAD')
     .toString()
     .trim()
-    .substring(0, 7)
-    .split('/')
-    .shift();
+    .substring(0, 7);
 const branch = execSync('git rev-parse --abbrev-ref HEAD')
     .toString()
-    .trim();
+    .trim()
+    .split('/')
+    .shift();
 
 execSync(
     `lerna publish prepatch --force-publish --yes --exact --no-push --no-git-reset --preid=${branch}.${commit} --registry=https://npm.pkg.github.com`
