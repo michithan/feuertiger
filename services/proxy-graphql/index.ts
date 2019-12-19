3d604552a95e9c4dd2c7d1f54be397713295c087
MonoMichi <michael.thanei@gmail.com>
2019-12-19 22:00:49 +0100

    merge

---
+++ services/proxy-graphql/index.ts
@@ -0,0 +1,53 @@
+import { ApolloServer } from "apollo-server";
+
+import { TyperesolverDirective } from "@feuertiger/schema-graphql";
+import { Node, Edge, Connection } from "@feuertiger/schema-graphql";
+
+import { requireGraphql } from "./utils";
+import { GetNode } from "./resolver";
+
+const schema = requireGraphql("./schema.graphql");
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  schemaDirectives: {
+    typeresolver: TyperesolverDirective
+  },
+  resolvers: {
+    Query: {
+      node: GetNode
+    },
+    Node: {
+      __resolveType: (source: Node, context: any) => {
+        const { id } = source;
+
+        const type = id.split(":").shift();
+
+        console.log("source: ", source);
+        console.log("context: ", context);
+
+        if (!type) {
+          throw Error(`invalid globalid: ${id}`);
+        }
+
+        return type;
+      }
+    },
+    Edge: {
+      __resolveType: () => (source: Edge, context: any) => {
+        console.log("source: ", source);
+        console.log("context: ", context);
+      }
+    },
+    Connection: {
+      __resolveType: () => (source: Connection, context: any) => {
+        console.log("source: ", source);
+        console.log("context: ", context);
+      }
+    }
+  }
+});
+
+server.listen().then(({ url }: { url: string }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
