d2647c0999cbd6b4033dc9ac843279a97f6d6471
Monomichi <michael.thanei@gmail.com>
2019-12-19 09:31:56 +0100

    Update publish.js

--- scripts/publish.js
+++ scripts/publish.js
@@ -11,5 +11,5 @@ const branch = execSync('git rev-parse --abbrev-ref HEAD')
     .shift();
 
 execSync(
-    `lerna publish prerelease --yes --exact --no-push --preid=${branch}.${commit} --registry=https://npm.pkg.github.com`
+    `lerna publish prerelease --yes --exact --amend --preid=${branch}.${commit} --registry=https://npm.pkg.github.com`
 );
