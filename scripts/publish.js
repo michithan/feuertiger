c6cb994fd1eec53138e97794b4aa736fdf49e817
MonoMichi <michael.thanei@gmail.com>
2019-12-19 21:51:13 +0100

    v1.0.9-dev.dae824a.0

--- scripts/publish.js
+++ scripts/publish.js
@@ -10,8 +10,12 @@ const branch = execSync('git rev-parse --abbrev-ref HEAD')
     .split('/')
     .shift();
 
-execSync(
-    `lerna publish prerelease --yes --exact --amend --preid=${branch}.${commit} --registry=https://npm.pkg.github.com`
+const changes = JSON.parse(
+    execSync('lerna list --since --json --no-private').toString()
 );
-execSync(`git commit --amend -m "publish\n\n\nskip-checks: true"`);
-execSync(`git push --force-with-lease`);
+
+if (changes.length > 0) {
+    execSync(
+        `lerna publish prerelease --yes --exact --preid=${branch}.${commit} --registry=https://npm.pkg.github.com`
+    );
+}
