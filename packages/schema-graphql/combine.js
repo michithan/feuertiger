1677020691ca993cb484e57d38a56abd1fa9ce47
feuertiger <michael.thanei@gmail.com>
2019-12-20 21:24:15 +0100

    try to fix graphql-backend

--- packages/schema-graphql/combine.js
+++ packages/schema-graphql/combine.js
@@ -3,9 +3,9 @@ const { importSchema } = require('graphql-import');
 
 const modulschema = require.resolve('./src/root.graphql');
 
-console.log('modulschema:', modulschema);
-
 const schema = importSchema(modulschema);
 
-fs.existsSync('./dist') || fs.mkdirSync('./dist');
+if (!fs.existsSync('./dist')) {
+    fs.mkdirSync('./dist');
+}
 fs.writeFileSync('./dist/schema.graphql', schema, 'utf8');
