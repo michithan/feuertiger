e531fe5dd2da0943c38d6b58f23d2fbc1f99cc7e
feuertiger <michael.thanei@gmail.com>
2019-12-20 18:28:21 +0100

    fixed firebase function deploy

---
+++ deployments/firebase/functions/lib/index.d.ts
@@ -0,0 +1,3 @@
+import * as functions from 'firebase-functions';
+export declare const graphql: functions.HttpsFunction;
+//# sourceMappingURL=index.d.ts.map
--- deployments/firebase/functions/src/index.ts
+++ deployments/firebase/functions/src/index.ts
@@ -1,7 +1,10 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
 import { gqlServer } from '@feuertiger/proxy-graphql';
 
+admin.initializeApp();
+
 const server = gqlServer();
 
-export const helloWorld = functions.https.onRequest(server);
+export const graphql = functions.https.onRequest(server);
