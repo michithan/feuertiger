a19fb318e82ba719436559536f22b998c15da195
feuertiger <michael.thanei@gmail.com>
2019-12-21 13:45:11 +0100

    modifyed env stlye config to firebase style config for firebase config

--- packages/tools/src/secrets.ts
+++ packages/tools/src/secrets.ts
@@ -1,38 +1,37 @@
 import { ServiceAccount } from 'firebase-admin';
 import functions from 'firebase-functions';
 
-import { getSecretsEnvironment, SecretsEnvironment } from './environment';
+import { getSecretsEnvironment } from './environment';
 
 const config =
-    (functions && functions.config() && functions.config().env) || {};
+    (functions && functions.config() && functions.config().secrets) || {};
 
 export const getFirebaseAdminSecrets = () => {
     const {
         FIREBASE_SECRETS_PROJECT_ID,
         FIREBASE_SECRETS_PRIVATE_KEY,
         FIREBASE_SECRETS_CLIENT_EMAIL
-    } = {
-        ...getSecretsEnvironment(),
-        ...config
-    } as SecretsEnvironment;
+    } = getSecretsEnvironment();
 
     const secrets: ServiceAccount = {
         projectId: FIREBASE_SECRETS_PROJECT_ID,
         privateKey: FIREBASE_SECRETS_PRIVATE_KEY,
-        clientEmail: FIREBASE_SECRETS_CLIENT_EMAIL
+        clientEmail: FIREBASE_SECRETS_CLIENT_EMAIL,
+        ...config
     };
     return secrets;
 };
 
 export const getFirebaseAppSecrets = () => {
-    const { FIREBASE_SECRETS_APP_AUTHDOMAIN, FIREBASE_SECRETS_APP_APIKEY } = {
-        ...getSecretsEnvironment(),
-        ...config
-    } as SecretsEnvironment;
+    const {
+        FIREBASE_SECRETS_APP_AUTHDOMAIN,
+        FIREBASE_SECRETS_APP_APIKEY
+    } = getSecretsEnvironment();
 
     const tokens = {
         apiKey: FIREBASE_SECRETS_APP_APIKEY,
-        authDomain: FIREBASE_SECRETS_APP_AUTHDOMAIN
+        authDomain: FIREBASE_SECRETS_APP_AUTHDOMAIN,
+        ...config
     };
 
     return tokens;
