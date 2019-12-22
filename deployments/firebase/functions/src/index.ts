e3bc90fea382bf9922a14ab20a30e869688b152b
feuertiger <michael.thanei@gmail.com>
2019-12-22 15:53:10 +0100

    just init app once

--- deployments/firebase/functions/src/index.ts
+++ deployments/firebase/functions/src/index.ts
@@ -1,9 +1,6 @@
 import * as functions from 'firebase-functions';
-import admin from 'firebase-admin';
 
 import { gqlServer } from '@feuertiger/proxy-graphql';
-
-admin.initializeApp();
 
 const server = gqlServer();
 
