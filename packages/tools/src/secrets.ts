06eff3e0cd60f830b27987229407a5dd326b4b0c
feuertiger <michael.thanei@gmail.com>
2019-12-22 11:05:29 +0100

    next try fixing deploy config

--- packages/tools/src/secrets.ts
+++ packages/tools/src/secrets.ts
@@ -3,8 +3,20 @@ import functions from 'firebase-functions';
 
 import { getSecretsEnvironment } from './environment';
 
-const config =
-    (functions && functions.config() && functions.config().secrets) || {};
+const getFirebaseConfig = () => {
+    const config =
+        functions && functions.config() && functions.config().secrets;
+    if (config) {
+        return {
+            projectId: config.projectid,
+            privateKey: config.privatekey,
+            clientEmail: config.clientemail,
+            apiKey: config.apikey,
+            authDomain: config.authdomain
+        };
+    }
+    return {};
+};
 
 export const getFirebaseAdminSecrets = () => {
     const {
@@ -17,7 +29,7 @@ export const getFirebaseAdminSecrets = () => {
         projectId: FIREBASE_SECRETS_PROJECT_ID,
         privateKey: FIREBASE_SECRETS_PRIVATE_KEY,
         clientEmail: FIREBASE_SECRETS_CLIENT_EMAIL,
-        ...config
+        ...getFirebaseConfig()
     };
     return secrets;
 };
@@ -31,7 +43,7 @@ export const getFirebaseAppSecrets = () => {
     const tokens = {
         apiKey: FIREBASE_SECRETS_APP_APIKEY,
         authDomain: FIREBASE_SECRETS_APP_AUTHDOMAIN,
-        ...config
+        ...getFirebaseConfig()
     };
 
     return tokens;
