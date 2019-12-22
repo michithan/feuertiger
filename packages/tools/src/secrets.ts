60473606380b5090fec026fd1b445cdbb3d29413
feuertiger <michael.thanei@gmail.com>
2019-12-22 15:09:44 +0100

    fixed sercetv resolve behavior

--- packages/tools/src/secrets.ts
+++ packages/tools/src/secrets.ts
@@ -1,21 +1,27 @@
-import { ServiceAccount } from 'firebase-admin';
 import functions from 'firebase-functions';
 
 import { getSecretsEnvironment } from './environment';
 
-const getFirebaseConfig = () => {
-    const config = functions && functions.config();
-    const secrets = (config && config.secrets) || {};
-    if (secrets) {
-        return {
-            projectId: secrets.projectid,
-            privateKey: secrets.privatekey,
-            clientEmail: secrets.clientemail,
-            apiKey: secrets.apikey,
-            authDomain: secrets.authdomain
-        };
-    }
-    return {};
+interface Config {
+    [key: string]: string | undefined;
+}
+
+const AddFirebaseConfig = (config: Config): Config => {
+    const secrets = functions?.config()?.secrets;
+    const firebaseConfig: Config = {
+        projectId: secrets?.projectid,
+        privateKey: secrets?.privatekey,
+        clientEmail: secrets?.clientemail,
+        apiKey: secrets?.apikey,
+        authDomain: secrets?.authdomain
+    };
+    const nextConfig: Config = { ...config };
+    Object.keys(nextConfig).forEach((key: string) => {
+        if (firebaseConfig[key]) {
+            nextConfig[key.toString()] = firebaseConfig[key];
+        }
+    });
+    return nextConfig;
 };
 
 export const getFirebaseAdminSecrets = () => {
@@ -24,13 +30,11 @@ export const getFirebaseAdminSecrets = () => {
         FIREBASE_SECRETS_PRIVATE_KEY,
         FIREBASE_SECRETS_CLIENT_EMAIL
     } = getSecretsEnvironment();
-    const { projectId, privateKey, clientEmail } = {
+    return AddFirebaseConfig({
         projectId: FIREBASE_SECRETS_PROJECT_ID,
         privateKey: FIREBASE_SECRETS_PRIVATE_KEY,
-        clientEmail: FIREBASE_SECRETS_CLIENT_EMAIL,
-        ...getFirebaseConfig()
-    };
-    return { projectId, privateKey, clientEmail } as ServiceAccount;
+        clientEmail: FIREBASE_SECRETS_CLIENT_EMAIL
+    });
 };
 
 export const getFirebaseAppSecrets = () => {
@@ -38,10 +42,8 @@ export const getFirebaseAppSecrets = () => {
         FIREBASE_SECRETS_APP_AUTHDOMAIN,
         FIREBASE_SECRETS_APP_APIKEY
     } = getSecretsEnvironment();
-    const { apiKey, authDomain } = {
+    return AddFirebaseConfig({
         apiKey: FIREBASE_SECRETS_APP_APIKEY,
-        authDomain: FIREBASE_SECRETS_APP_AUTHDOMAIN,
-        ...getFirebaseConfig()
-    };
-    return { apiKey, authDomain };
+        authDomain: FIREBASE_SECRETS_APP_AUTHDOMAIN
+    });
 };
