2603149e1a3cee24d1291f38ff2b8c2479a99a99
feuertiger <michael.thanei@gmail.com>
2019-12-22 13:42:49 +0100

    improved env sercrets seucirty

--- packages/tools/src/secrets.ts
+++ packages/tools/src/secrets.ts
@@ -4,15 +4,15 @@ import functions from 'firebase-functions';
 import { getSecretsEnvironment } from './environment';
 
 const getFirebaseConfig = () => {
-    const config =
-        functions && functions.config() && functions.config().secrets;
-    if (config) {
+    const config = functions && functions.config();
+    const secrets = (config && config.secrets) || {};
+    if (secrets) {
         return {
-            projectId: config.projectid,
-            privateKey: config.privatekey,
-            clientEmail: config.clientemail,
-            apiKey: config.apikey,
-            authDomain: config.authdomain
+            projectId: secrets.projectid,
+            privateKey: secrets.privatekey,
+            clientEmail: secrets.clientemail,
+            apiKey: secrets.apikey,
+            authDomain: secrets.authdomain
         };
     }
     return {};
@@ -24,14 +24,13 @@ export const getFirebaseAdminSecrets = () => {
         FIREBASE_SECRETS_PRIVATE_KEY,
         FIREBASE_SECRETS_CLIENT_EMAIL
     } = getSecretsEnvironment();
-
-    const secrets: ServiceAccount = {
+    const { projectId, privateKey, clientEmail } = {
         projectId: FIREBASE_SECRETS_PROJECT_ID,
         privateKey: FIREBASE_SECRETS_PRIVATE_KEY,
         clientEmail: FIREBASE_SECRETS_CLIENT_EMAIL,
         ...getFirebaseConfig()
     };
-    return secrets;
+    return { projectId, privateKey, clientEmail } as ServiceAccount;
 };
 
 export const getFirebaseAppSecrets = () => {
@@ -39,12 +38,10 @@ export const getFirebaseAppSecrets = () => {
         FIREBASE_SECRETS_APP_AUTHDOMAIN,
         FIREBASE_SECRETS_APP_APIKEY
     } = getSecretsEnvironment();
-
-    const tokens = {
+    const { apiKey, authDomain } = {
         apiKey: FIREBASE_SECRETS_APP_APIKEY,
         authDomain: FIREBASE_SECRETS_APP_AUTHDOMAIN,
         ...getFirebaseConfig()
     };
-
-    return tokens;
+    return { apiKey, authDomain };
 };
--- services/proxy-graphql/src/resolver.ts
+++ services/proxy-graphql/src/resolver.ts
@@ -11,6 +11,7 @@ import {
 import { ParseId } from '@feuertiger/utils-graphql';
 import { getFirebaseAdminSecrets } from '@feuertiger/tools';
 
+console.log('getFirebaseAdminSecrets()', getFirebaseAdminSecrets());
 const db = initDb(getFirebaseAdminSecrets() as ServiceAccount);
 seed(db);
 
