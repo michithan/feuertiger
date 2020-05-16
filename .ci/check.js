bf4306b40bf5b7312b0e869d64bdfff286f01f15
Monomichi <michael.thanei@googlemail.com>
2020-05-16 18:37:42 +0000

    Update .ci/2-publish.yml, .ci/1-test.yml, .ci/3-deploy.yml, .ci/0-prepare.yml, .ci/check.js, .ci/1-format.yml, .gitlab-ci.yml, .ci/1-check.yml files

---
+++ .ci/check.js
@@ -0,0 +1,24 @@
+const { execSync } = require('child_process');
+
+execSync('yarn');
+
+execSync('yarn linkdist');
+
+execSync('yarn format');
+
+const hasChanges = () => JSON.parse(execSync('git diff-index --quiet HEAD || echo true').toString());
+
+if (hasChanges()) {
+
+    execSync('git commit -am "format & fix');
+
+    execSync('git push');
+
+} else {
+
+    const changes = JSON.parse(
+        execSync('lerna list --since --json --no-private').toString()
+    );
+
+    console.log("changes: ", changes);
+}
