bbc4cd64991581f2ae4f0757f5300164e3e1f88e
Monomichi <michael.thanei@googlemail.com>
2020-05-16 19:05:02 +0000

    Update .ci/check.js

--- .ci/check.js
+++ .ci/check.js
@@ -1,22 +1,30 @@
 const { execSync } = require('child_process');
 
-execSync('yarn linkdist');
+console.log('Linking dist folders');
+execSync('yarn linkdist', { stdio: 'inherit' });
 
-execSync('yarn format');
+console.log('Enforce code formatting');
+execSync('yarn format', { stdio: 'inherit' });
 
-const hasChanges = () => JSON.parse(execSync('git diff-index --quiet HEAD || echo true').toString().trim());
+const hasChanges = () =>
+    JSON.parse(
+        execSync('git diff-index --quiet HEAD || echo true').toString().trim()
+    );
 
 if (hasChanges()) {
+    console.log('Commit code formatting changes');
 
-    execSync('git commit -am "format & fix');
+    execSync('git commit -am "format & fix', { stdio: 'inherit' });
 
-    execSync('git push');
-
+    execSync('git push', { stdio: 'inherit' });
 } else {
+    console.log('Check changes');
 
     const changes = JSON.parse(
         execSync('lerna list --since --json --no-private').toString()
     );
 
-    console.log("changes: ", changes);
+    console.log('changes: ', changes);
+
+    console.log('Plan build steps');
 }
