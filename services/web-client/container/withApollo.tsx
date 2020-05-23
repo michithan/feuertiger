a20b5dd392851585bda056389c808fe6606c975b
michael.thanei <michael.thanei@gmail.com>
2020-05-23 09:41:56 +0000

    readded apollo client auth

--- services/web-client/container/withApollo.tsx
+++ services/web-client/container/withApollo.tsx
@@ -5,18 +5,18 @@ import { setContext } from 'apollo-link-context';
 import withApollo from 'next-with-apollo';
 import fetch from 'isomorphic-unfetch';
 
-// import AuthSingleton from './authSingleton';
+import AuthSingleton from './authSingleton';
 
 // Update the GraphQL endpoint to any instance of GraphQL that you like
 const GRAPHQL_URL = 'http://localhost:4000/';
 
 const authLink = setContext(async (_, { headers }) => {
     try {
-        // const token = await new AuthSingleton().firebaseAuth.currentUser.getIdToken();
+        const token = await new AuthSingleton().firebaseAuth.currentUser.getIdToken();
         return {
             headers: {
-                ...headers
-                // authorization: token
+                ...headers,
+                authorization: token
             }
         };
     } catch (error) {
