5de7a648beae554908a4408802f6b66871836a76
michael.thanei <michael.thanei@gmail.com>
2020-07-16 19:50:48 +0000

    gitlab ci

--- infrastructure/gitlab/cluster.ts
+++ infrastructure/gitlab/cluster.ts
@@ -1,11 +1,13 @@
 import * as gitlab from '@pulumi/gitlab';
 
 import { cluster as doCluster, token, cert } from '../digitalocean/cluster';
+import { loadBalancer } from '../digitalocean/loadBalancer';
 import { project } from './project';
 
 export const cluster = new gitlab.ProjectCluster('feuer-cluster', {
     enabled: true,
     environmentScope: '*',
+    domain: `${loadBalancer}.nip.io`,
     kubernetesApiUrl: doCluster.endpoint,
     kubernetesCaCert: cert,
     managed: true,
