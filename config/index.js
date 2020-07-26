555712f352b0dc122a0ee8ad999372e4379c7215
michael.thanei <michael.thanei@gmail.com>
2020-07-26 19:48:26 +0000

    config.js and fixed production build stuff

--- config/index.js
+++ config/index.js
@@ -4,16 +4,33 @@ const constants = require('./constants.json');
 /*
  * Get all existing environment variables
  */
-const FIREBASE_CONFIG = JSON.parse(process.env.FIREBASE_CONFIG);
-const GOOGLE_CREDENTIALS = JSON.parse(process.env.GOOGLE_CREDENTIALS);
-
 const {
     POSTGRES_URI,
     GRAPHQL_URI,
     WEB_CLIENT_URI,
     DIGITALOCEAN_TOKEN,
-    GIT_TOKEN
+    GIT_TOKEN,
+    GIT_USER
 } = process.env;
+
+const FIREBASE_CONFIG = JSON.parse(process.env.FIREBASE_CONFIG || '{}');
+const GOOGLE_CREDENTIALS = JSON.parse(process.env.GOOGLE_CREDENTIALS || '{}');
+
+const tryGetGitBranchSlug = () =>
+    execSync('git branch --show-current || echo none', {
+        stdio: ['pipe', 'pipe', 'ignore']
+    })
+        .toString('utf-8')
+        .trim()
+        .toLowerCase()
+        .replace(/[^a-z^0-9]/g, '-');
+
+const tryGetGitCommitHashShort = () =>
+    execSync('git rev-parse --short HEAD || echo none', {
+        stdio: ['pipe', 'pipe', 'ignore']
+    })
+        .toString('utf-8')
+        .trim();
 
 /*
  * Compute configs
@@ -27,39 +44,21 @@ exports = {
     },
     gcp: constants.gcp,
     digitaloceanToken: DIGITALOCEAN_TOKEN,
-    gitBranchSlug: execSync('git branch --show-current')
-        .toString('utf-8')
-        .trim()
-        .toLowerCase()
-        .replace(/[^a-z^0-9]/g, '-'),
-    gitCommitHashShort: execSync('git rev-parse --short HEAD')
-        .toString('utf-8')
-        .trim(),
+    gitBranchSlug: tryGetGitBranchSlug(),
+    gitCommitHashShort: tryGetGitCommitHashShort(),
     firebaseAppConfig: {
-        projectId: FIREBASE_CONFIG.project_id,
-        apiKey: FIREBASE_CONFIG.apiKey,
-        authDomain: FIREBASE_CONFIG.authDomain
+        projectId: FIREBASE_CONFIG && FIREBASE_CONFIG.project_id,
+        apiKey: FIREBASE_CONFIG && FIREBASE_CONFIG.apiKey,
+        authDomain: FIREBASE_CONFIG && FIREBASE_CONFIG.authDomain
     },
     firebaseAdminConfig: {
-        projectId: GOOGLE_CREDENTIALS.project_id,
-        privateKey: GOOGLE_CREDENTIALS.private_key,
-        clientEmail: GOOGLE_CREDENTIALS.client_email
+        projectId: GOOGLE_CREDENTIALS && GOOGLE_CREDENTIALS.project_id,
+        privateKey: GOOGLE_CREDENTIALS && GOOGLE_CREDENTIALS.private_key,
+        clientEmail: GOOGLE_CREDENTIALS && GOOGLE_CREDENTIALS.client_email
     },
     postgresUri:
         POSTGRES_URI ||
         'postgresql://feuertiger:feuertiger@localhost:5432/feuertiger',
     graphqlUri: GRAPHQL_URI || 'http://localhost:4000/',
-    webClientUri: WEB_CLIENT_URI || 'http://localhost:3000'
+    webClientUri: WEB_CLIENT_URI || 'http://localhost:3000/'
 };
-
-/*
- * Re-export all deafault environment variables
- */
-execSync(`export WEB_CLIENT_URI=${exports.webClientUri}`);
-execSync(`export GRAPHQL_URI=${exports.graphqlUri}`);
-execSync(`export POSTGRES_URI=${exports.postgresUri}`);
-
-execSync(`export CYPRESS_BASE_URL=${exports.webClientUri}`);
-
-execSync(`export GIT_BRANCH_SLUG=${exports.gitBranchSlug}`);
-execSync(`export GIT_COMMIT_HASH_SHORT=${exports.gitCommitHashShort}`);
