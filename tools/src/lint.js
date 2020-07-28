d0e084cbf6ff3bbd1ddee39e32185382f4201a2a
michael.thanei <michael.thanei@gmail.com>
2020-07-28 20:30:43 +0000

    eslint update

--- tools/src/lint.js
+++ tools/src/lint.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const exec = require('@lerna/exec');
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, '..', '..');
 
 exec({
     cmd: `eslint ./**/*.{ts,tsx} --fix --ignore-path ${root}/.eslintignore --config  ${root}/.eslintrc --resolve-plugins-relative-to  ${root}/ --no-error-on-unmatched-pattern`,
