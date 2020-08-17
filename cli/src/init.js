2f0ce074427a6bb07717bf305f43c1a5dc9ef435
michael.thanei <michael.thanei@gmail.com>
2020-08-17 09:12:08 +0000

    dev env performance improvments

--- cli/src/init.js
+++ cli/src/init.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-const { writeFileSync } = require('fs');
+const { writeFileSync, existsSync } = require('fs');
 const execa = require('execa');
 const config = require('@feuertiger/config');
 const { root } = require('./paths');
@@ -13,35 +13,63 @@ const dbCreateUser = asPostgres(
 
 const dbCreateDb = asPostgres('createdb -O feuertiger feuertiger');
 
-module.exports = async flags => {
-    // console.log('installing dependencies');
-    // await execa('yarn', [], {
-    //     cwd: root,
-    //     stdout: 'inherit'
-    // });
+const installDependencies = async () => {
+    console.log('installing dependencies');
+    await execa('yarn', [], {
+        cwd: root,
+        stdout: 'inherit'
+    });
+};
 
-    // console.log('linking dist folders');
-    // await linkdist();
+const linkDistFolders = async () => {
+    console.log('linking dist folders');
+    await linkdist();
+};
 
-    // console.log('setup postgresql database');
-    // try {
-    //     execSync('touch /run/openrc/softlevel', {
-    //         stdio: ['inherit', 'ignore', 'ignore']
-    //     });
-    //     execSync('rc-service postgresql start', {
-    //         stdio: ['inherit', 'ignore', 'ignore']
-    //     });
-    //     execSync(dbCreateUser, {
-    //         stdio: ['inherit', 'ignore', 'ignore']
-    //     });
-    //     execSync(dbCreateDb, {
-    //         stdio: ['inherit', 'ignore', 'ignore']
-    //     });
-    //     console.log('database setup finished ');
-    // } catch (error) {
-    //     console.log('database already setup');
-    // }
+const setupDb = async () => {
+    console.log('setup postgresql database');
 
+    const openrcSoftlevelPath = '/run/openrc/softlevel';
+    if (!existsSync(openrcSoftlevelPath)) {
+        execSync(`touch ${openrcSoftlevelPath}`, {
+            stdio: ['inherit', 'ignore', 'ignore']
+        });
+    }
+
+    try {
+        execSync('rc-service postgresql start', {
+            stdio: ['inherit', 'ignore', 'ignore']
+        });
+        console.log('started postgresql service');
+    } catch (error) {
+        console.error(error);
+        console.log('database service already started');
+    }
+
+    try {
+        execSync(dbCreateUser, {
+            stdio: ['inherit', 'ignore', 'ignore']
+        });
+        console.log('created database user');
+    } catch (error) {
+        console.error(error);
+        console.log('database user already created');
+    }
+
+    try {
+        execSync(dbCreateDb, {
+            stdio: ['inherit', 'ignore', 'ignore']
+        });
+        console.log('created database');
+    } catch (error) {
+        console.error(error);
+        console.log('database already created');
+    }
+
+    console.log('database setup finished ');
+};
+
+const syncEnv = async () => {
     console.log('sync project config with env');
     console.log(config.env);
 
@@ -51,4 +79,11 @@ module.exports = async flags => {
     );
     console.log(exports);
     writeFileSync(`/etc/profile.d/env.sh`, exports);
+};
+
+module.exports = async flags => {
+    await installDependencies();
+    await linkDistFolders();
+    await setupDb();
+    await syncEnv();
 };
