bccd8f5ffcce1f1e732547d0ee824984077ec0ce
michael.thanei <michael.thanei@gmail.com>
2020-10-06 19:46:01 +0000

    some cleanups

--- services/graphql/src/startup.ts
+++ services/graphql/src/startup.ts
@@ -1,26 +1,23 @@
-(async () => {
-    // eslint-disable-next-line global-require
-    const firebase = require('firebase-admin');
-    // eslint-disable-next-line global-require
-    const { firebaseAdminConfig, graphqlUri } = require('@feuertiger/config');
-    const [arg, number] = process.argv.slice(2);
-    const defaultPort = 8080;
-    const port = (arg === '--port' && number) || defaultPort;
-    try {
-        await firebase.initializeApp(firebaseAdminConfig);
-        // eslint-disable-next-line global-require
-        require('./server')
-            .gqlServer()
-            .listen({ port }, () =>
-                // eslint-disable-next-line no-console
-                console.log(
-                    `🚀 Server ready at ${
-                        port === defaultPort ? graphqlUri : `port ${port}`
-                    }`
-                )
-            );
-    } catch (error) {
+import firebase from 'firebase-admin';
+import config from '@feuertiger/config';
+import * as server from './server';
+
+const [arg, number] = process.argv.slice(2);
+const defaultPort = 4000;
+const port = (arg === '--port' && number) || defaultPort;
+const { firebaseAdminConfig, graphqlUri } = config;
+
+try {
+    firebase.initializeApp(firebaseAdminConfig);
+    server.gqlServer().listen({ port }, () =>
         // eslint-disable-next-line no-console
-        console.log('error: ', error);
-    }
-})();
+        console.log(
+            `🚀 Server ready at ${
+                port === defaultPort ? graphqlUri : `port ${port}`
+            }`
+        )
+    );
+} catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('error: ', error);
+}
