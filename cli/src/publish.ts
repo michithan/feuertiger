acf7f0f19a9d4912692851d0b6f07408d1402a6f
michael.thanei <michael.thanei@gmail.com>
2020-10-11 12:18:16 +0000

    npm login

--- cli/src/publish.ts
+++ cli/src/publish.ts
@@ -4,7 +4,7 @@ import { gitlab, git, npmRegistry } from '@feuertiger/config';
 export default async (): Promise<void> => {
     const { branchSlug, commit } = git;
     const { token } = gitlab;
-    const npmLogin = `npm config set '${npmRegistry}:_authToken' "${token}"`;
+    const npmLogin = `npm config set ${npmRegistry}:_authToken ${token}`;
     const lernaPublish = `lerna publish prerelease --no-push --yes --exact --preid=${branchSlug}.${commit}`;
     try {
         await execa.command(`${npmLogin} && ${lernaPublish}`, {
