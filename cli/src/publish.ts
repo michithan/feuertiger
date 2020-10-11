0e699a55affbd1eac097408bbb1adcb8afff12af
michael.thanei <michael.thanei@gmail.com>
2020-10-11 12:40:17 +0000

    adde .npmrc for auth

--- cli/src/publish.ts
+++ cli/src/publish.ts
@@ -1,17 +1,25 @@
 import execa from 'execa';
-import { gitlab, git, npmRegistry } from '@feuertiger/config';
+import { git, npmRegistry } from '@feuertiger/config';
 
 export default async (): Promise<void> => {
     const { branchSlug, commit } = git;
-    const { token } = gitlab;
-    const npmLogin = `npm config set ${npmRegistry}:_authToken ${token}`;
-    const lernaPublish = `lerna publish prerelease --no-push --yes --exact --preid=${branchSlug}.${commit}`;
     try {
-        await execa.command(`${npmLogin} && ${lernaPublish}`, {
-            shell: true,
-            stdout: 'inherit',
-            stderr: 'inherit'
-        });
+        await execa(
+            'lerna',
+            [
+                'publish',
+                'prerelease',
+                '--no-push',
+                '--yes',
+                '--exact',
+                `--preid=${branchSlug}.${commit}`,
+                `--registry=${npmRegistry}`
+            ],
+            {
+                stdout: 'inherit',
+                stderr: 'inherit'
+            }
+        );
     } catch (error) {
         console.log(error);
     }
