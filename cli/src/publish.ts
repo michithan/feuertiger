fa391c59bf0b01e7ecae89aa7054962616af6f55
michael.thanei <michael.thanei@gmail.com>
2020-10-11 12:11:33 +0000

    fix npm login

--- cli/src/publish.ts
+++ cli/src/publish.ts
@@ -4,31 +4,14 @@ import { gitlab, git, npmRegistry } from '@feuertiger/config';
 export default async (): Promise<void> => {
     const { branchSlug, commit } = git;
     const { token } = gitlab;
+    const npmLogin = `npm config set '${npmRegistry}:_authToken' "${token}"`;
+    const lernaPublish = `lerna publish prerelease --no-push --yes --exact --preid=${branchSlug}.${commit}`;
     try {
-        await execa(
-            'npm',
-            ['config', 'set', `'${npmRegistry}:_authToken' "${token}"`],
-            {
-                stdout: 'inherit',
-                stderr: 'inherit'
-            }
-        );
-        await execa(
-            'lerna',
-            [
-                'publish',
-                'prerelease',
-                '--no-push',
-                '--yes',
-                '--exact',
-                `--preid=${branchSlug}.${commit}`,
-                `--registry=${npmRegistry}`
-            ],
-            {
-                stdout: 'inherit',
-                stderr: 'inherit'
-            }
-        );
+        await execa.command(`${npmLogin} && ${lernaPublish}`, {
+            shell: true,
+            stdout: 'inherit',
+            stderr: 'inherit'
+        });
     } catch (error) {
         console.log(error);
     }
