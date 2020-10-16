4cb30d4af312c555cfa230986f601baedb7e906a
michael.thanei <michael.thanei@gmail.com>
2020-10-16 17:51:08 +0000

    update all package versions

--- packages/web-components/.storybook/main.js
+++ packages/web-components/.storybook/main.js
@@ -1,3 +1,25 @@
+const babelConfig = require('../babel.config');
+
 module.exports = {
-    stories: ['../src/**/*.stories.tsx']
+    stories: ['../src/**/*.stories.tsx'],
+    babel: async options => {
+        options.plugins.push(...babelConfig.plugins);
+        return options;
+    },
+    webpackFinal: async config => {
+        config.resolve.extensions.push('.js', '.ts', '.tsx');
+        config.resolve.alias = {
+            '@feuertiger/ocr': require.resolve('@feuertiger/ocr'),
+            '@feuertiger/schema-graphql': require.resolve(
+                '@feuertiger/schema-graphql'
+            )
+        };
+        config.node = {
+            fs: 'empty',
+            child_process: 'empty',
+            net: 'empty',
+            tls: 'empty'
+        };
+        return config;
+    }
 };
