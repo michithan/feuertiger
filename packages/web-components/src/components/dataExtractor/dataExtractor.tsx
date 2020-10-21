cccbd4002f8a09a555a37e9f3aeaf445af366335
michael.thanei <michael.thanei@gmail.com>
2020-10-21 20:30:08 +0000

    fix lint error

--- packages/web-components/src/components/dataExtractor/dataExtractor.tsx
+++ packages/web-components/src/components/dataExtractor/dataExtractor.tsx
@@ -12,6 +12,7 @@ import { FileInfo } from './fileInfo';
 import { TextData } from './textData';
 import { CleaningOptions } from './cleaningOptions';
 import { ParsingOptions } from './parsingOptions';
+import { FlattData } from './flattData';
 
 interface State {
     file: File | null | undefined;
