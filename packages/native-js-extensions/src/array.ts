5cdb2d439f0c47ee5ca17eb1727f178196d836fa
michael.thanei <michael.thanei@gmail.com>
2021-02-03 20:26:51 +0000

    upgrade

--- packages/native-js-extensions/src/array.ts
+++ packages/native-js-extensions/src/array.ts
@@ -6,6 +6,7 @@ declare interface Array<T> {
     filterFalsy(): Array<T>;
     first(): T | undefined;
     last(): T | undefined;
+    filterAsync(predicate: (element: T) => Promise<boolean>): Promise<Array<T>>;
 }
 
 Array.prototype.filterFalsy = function () {
@@ -18,4 +19,21 @@ Array.prototype.first = function () {
 
 Array.prototype.last = function () {
     return this[this.length - 1];
+};
+
+Array.prototype.filterAsync = async function <T>(
+    predicate: (element: T) => Promise<boolean>
+): Promise<Array<T>> {
+    const filtered: Array<T> = [];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const element of this) {
+        // eslint-disable-next-line no-await-in-loop
+        const keep = await predicate(element);
+        if (keep) {
+            filtered.push(element);
+        }
+    }
+
+    return filtered;
 };
