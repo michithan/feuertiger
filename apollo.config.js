b8155f5b77f7138e1f2bd7f47c3b016a8877e4ce
monomichi <michael.thanei@gmail.com>
2021-03-26 19:02:43 +0000

    fix apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,11 @@
-const { graphqlUri } = require('@feuertiger/config');
+// const { graphqlUri } = require('@feuertiger/config');
 
 module.exports = {
     client: {
         service: {
             name: 'graphql',
-            url: graphqlUri
+            // url: graphqlUri
+            localSchemaFile: './packages/schema-graphql/dist/schema.graphql'
         }
     }
 };
