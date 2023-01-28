fd3448f89c407773bbc8eba97384e2a90ab34c42
feuertiger <michael.thanei@gmail.com>
2023-01-28 21:34:14 +0100

    add Radzen

--- apps/tiger-web-app/Program.cs
+++ apps/tiger-web-app/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using tiger_web_app;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
 
 await builder.Build().RunAsync();
